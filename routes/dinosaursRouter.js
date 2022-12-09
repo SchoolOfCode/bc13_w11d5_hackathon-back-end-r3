9a750aabb674459dfaf94369d00d714dd7141e81
mvhmz81 <mvhmz@yahoo.co.uk>
2022-12-09 16:11:08 +0000

    working

--- routes/dinosaursRouter.js
+++ routes/dinosaursRouter.js
@@ -1,22 +1,23 @@
-import express from 'express';
+import express from "express";
 
-const dinosaursRouter = express.Router()
+const dinosaursRouter = express.Router();
 import {
-    getAllDinosaurs,
-    createDinosaurs,
-    // getDinosaursById,
-    // updateDinosaursById,
-    // deleteDinosaursById
-} from "../models/dinoModels.js"
+  getAllDinosaurs,
+  createDinosaurs,
+  // getDinosaursById,
+  // updateDinosaursById,
+  // deleteDinosaursById
+} from "../models/dinoModels.js";
 
-dinosaursRouter.get("/", async function (req, res){
-    const allDinosaurs = await getAllDinosaurs();
-    res.status(200).json(allDinosaurs);
+dinosaursRouter.get("/", async function (req, res) {
+  console.log("this is a get request");
+  const allDinosaurs = await getAllDinosaurs();
+  res.status(200).json(allDinosaurs);
 });
-dinosaursRouter.post("/", async function (req, res){
-    const newDinosaurs = req.body;
-    console.log(newDinosaurs);
-    const result = await createDinosaurs(newDinosaurs);
-    res.status(200).json(result);
+dinosaursRouter.post("/", async function (req, res) {
+  const newDinosaurs = req.body;
+  console.log(newDinosaurs);
+  const result = await createDinosaurs(newDinosaurs);
+  res.status(200).json(result);
 });
-export default dinosaursRouter
+export default dinosaursRouter;
